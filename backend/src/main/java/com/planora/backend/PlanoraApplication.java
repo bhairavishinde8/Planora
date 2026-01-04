6c49a41a9a2f43669412bcdaa980f54c36831c27
bhairavishinde8 <bhairavishinde8@gmail.com>
2026-01-04 23:14:09 +0530

    Day 1: Project setup completed

---
+++ backend/src/main/java/com/planora/backend/PlanoraApplication.java
@@ -0,0 +1,13 @@
+package com.planora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoraApplication.class, args);
+	}
+
+}
